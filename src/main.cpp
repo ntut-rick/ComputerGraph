6855d6d7d36afae1039e864ffc0bc8e570727dd6
ptsd <ptsd@ptsds-MacBook-Air.local>
2024-05-07 16:23:37 +0800

    something

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 
+#include <algorithm>
 #ifdef _MSC_VER
 #include "freeglut_std.h"
 #else
 #include <freeglut.h>
 #endif
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 #include <math.h>
 #include <stdio.h>
 
@@ -23,6 +28,10 @@ void ModelSelectMenuCallback(int);
 
 struct point {
   float x, y;
+};
+
+struct triangle {
+  point points[3];
 };
 
 GLenum glShadeType = GL_SMOOTH;
@@ -46,9 +55,10 @@ double deg2deg(double deg) { return deg; }
 int size = 10;
 const auto border = 0.05;
 
-#define POINT_COUNT 4
+#define MAX_POINT_COUNT 100
 
-point points[POINT_COUNT];
+triangle triangles[MAX_POINT_COUNT];
+int triangle_count = 0;
 int click_index = 0;
 
 void ScreenToView(int ix, int iy, double *ox, double *oy) {
@@ -60,8 +70,8 @@ void ScreenToView(int ix, int iy, double *ox, double *oy) {
   tx = (std::floor(tx) * 2 + 1.0) / size;
   ty = (std::floor(ty) * 2 + 1.0) / size;
 
-  *ox = tx;
-  *oy = ty;
+  *ox = tx + 1.0;
+  *oy = ty + 1.0;
 }
 
 void MouseHandler(int button, int state, int _x, int _y) {
@@ -70,17 +80,18 @@ void MouseHandler(int button, int state, int _x, int _y) {
   }
   // printf("%d\n", click_index);
 
-  if (click_index >= POINT_COUNT) {
-    return;
-  }
-
   double tx, ty;
   ScreenToView(_x, _y, &tx, &ty);
 
-  if (abs(tx) < 1.0 && abs(ty) < 1.0) {
-    points[click_index].x = tx;
-    points[click_index].y = ty;
+  if (tx <= 2.0 && ty <= 2.0) {
+    triangles[triangle_count].points[click_index].x = tx;
+    triangles[triangle_count].points[click_index].y = ty;
     click_index++;
+  }
+
+  if (click_index == 3) {
+    triangle_count++;
+    click_index = 0;
   }
 }
 
@@ -89,6 +100,7 @@ void SpecialKeyHandler(int key, int x, int y) {}
 void NormalKeyHandler(unsigned char key, int x, int y) {
   switch (key) {
   case 'r':
+    triangle_count = 0;
     click_index = 0;
     break;
   case 'x':
@@ -104,6 +116,7 @@ Model m;
 
 void Size(int value) {
   size = value;
+  triangle_count = 0;
   click_index = 0;
 }
 
@@ -187,13 +200,65 @@ void ChangeSize(int w, int h) {
   glLoadIdentity();
 }
 
+void DrawLine(point p1, point p2) {
+  const auto x1 = p1.x;
+  const auto y1 = p1.y;
+  const auto x2 = p2.x;
+  const auto y2 = p2.y;
+
+  const auto dx = abs(x2 - x1);
+  const auto dy = abs(y2 - y1);
+
+  const auto step = 2.0 / size;
+
+  const auto sign_x = (x2 - x1) > 0 ? step : -step;
+  const auto sign_y = (y2 - y1) > 0 ? step : -step;
+
+  auto x = x1;
+  auto y = y1;
+
+  float p;
+
+  glBegin(GL_POINTS);
+  if (dx > dy) {
+    p = 2 * dy - dx;
+    while (std::abs(x - x2) > 0.01) {
+      glVertex3f(x, y, 0);
+      x += sign_x;
+      if (p < 0)
+        p += 2 * dy;
+      else {
+        p += 2 * (dy - dx);
+        y += sign_y;
+      }
+    }
+  } else {
+    p = 2 * dx - dy;
+    while (std::abs(y - y2) > 0.01) {
+      glVertex3f(x, y, 0);
+      y += sign_y;
+      if (p < 0)
+        p += 2 * dx;
+      else {
+        p += 2 * (dx - dy);
+        x += sign_x;
+      }
+    }
+  }
+  glEnd();
+}
+
+bool isLeft(point p1, point p2, point p) {
+  return (p2.x - p1.x) * (p.y - p1.y) - (p2.y - p1.y) * (p.x - p1.x) > 0.0;
+}
+
 void RenderScene() {
   glClearColor(0, 0, 0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(-1 - border, 1 + border, -1 - border, 1 + border, -10, 10);
+  glOrtho(0 - border, 2 + border, 0 - border, 2 + border, -10, 10);
 
   glMatrixMode(GL_MODELVIEW);
   glEnable(GL_DEPTH_TEST);
@@ -203,85 +268,58 @@ void RenderScene() {
   glShadeModel(glShadeType);
 
   glBegin(GL_LINES);
-  for (float i = -1.0; i <= 1.0; i += 2.0 / size) {
-    glVertex3f(i, -1, 0);
-    glVertex3f(i, 1, 0);
+  for (double i = 0.0; i <= 2.0 + 0.01; i += 2.0 / size) {
+    glVertex3f(i, 0, 0);
+    glVertex3f(i, 2, 0);
   }
-  for (float i = -1.0; i <= 1.0; i += 2.0 / size) {
-    glVertex3f(-1, i, 0);
-    glVertex3f(1, i, 0);
+  for (double i = 0.0; i <= 2.0 + 0.01; i += 2.0 / size) {
+    glVertex3f(0, i, 0);
+    glVertex3f(2, i, 0);
   }
   glEnd();
 
   glColor3f(1.0, 0.0, 0.0);
   glPointSize((800.0 * (1.0 - border)) / size);
   glBegin(GL_POINTS);
-  for (int i = 0; i < click_index; ++i) {
-    glVertex3f(points[i].x, points[i].y, 0);
+  for (int i = 0; i < triangle_count * 3 + click_index; ++i) {
+    glVertex3f(triangles[i / 3].points[i % 3].x,
+               triangles[i / 3].points[i % 3].y, 0);
   }
   glEnd();
   glColor3f(1.0, 1.0, 1.0);
 
-  if (click_index == POINT_COUNT) {
-    for (int i = 0; i < click_index; ++i) {
-      printf("P%d: %f %f\n", i + 1, points[i].x, points[i].y);
-    }
+  // for (int i = 0; i < click_index; ++i) {
+  //   printf("P%d: %f %f\n", i + 1, points[i].x, points[i].y);
+  // }
 
-    for (int i = 0; i < click_index; ++i) {
-      const auto next = (i + 1) % click_index;
+  for (int i = 0; i < triangle_count; ++i) {
+    const auto p0 = triangles[i].points[0];
+    const auto p1 = triangles[i].points[1];
+    const auto p2 = triangles[i].points[2];
+    DrawLine(p0, p1);
+    DrawLine(p1, p2);
+    DrawLine(p2, p0);
 
-      printf("P%d-P%d: ", i + 1, next + 1);
-      print_info(points[i], points[next]);
+    const auto max_x = std::max({p0.x, p1.x, p2.x});
+    const auto max_y = std::max({p0.y, p1.y, p2.y});
+    const auto min_x = std::min({p0.x, p1.x, p2.x});
+    const auto min_y = std::min({p0.y, p1.y, p2.y});
 
-      const point begin = {points[i].x, points[i].y};
-      const point end = {points[next].x, points[next].y};
+    printf("%f %f %f %f\n", max_x, max_y, min_x, min_y);
 
-      const auto x1 = begin.x;
-      const auto y1 = begin.y;
-      const auto x2 = end.x;
-      const auto y2 = end.y;
-
-      const auto dx = abs(x2 - x1);
-      const auto dy = abs(y2 - y1);
-
-      const auto step = 2.0 / size;
-
-      const auto sign_x = (x2 - x1) > 0 ? step : -step;
-      const auto sign_y = (y2 - y1) > 0 ? step : -step;
-
-      auto x = x1;
-      auto y = y1;
-
-      float p;
-
-      glBegin(GL_POINTS);
-      if (dx > dy) {
-        p = 2 * dy - dx;
-        while (std::abs(x - x2) > 0.01) {
-          glVertex3f(x, y, 0);
-          x += sign_x;
-          if (p < 0)
-            p += 2 * dy;
-          else {
-            p += 2 * (dy - dx);
-            y += sign_y;
-          }
-        }
-      } else {
-        p = 2 * dx - dy;
-        while (std::abs(y - y2) > 0.01) {
-          glVertex3f(x, y, 0);
-          y += sign_y;
-          if (p < 0)
-            p += 2 * dx;
-          else {
-            p += 2 * (dx - dy);
-            x += sign_x;
-          }
+    glColor3f(0.0, 0.0, 1.0);
+    glPointSize((800.0 * (1.0 - border)) / size);
+    glBegin(GL_POINTS);
+    for (float x = min_x; x < max_x; x += 2.0 / size) {
+      for (float y = min_y; y < max_y; y += 2.0 / size) {
+        if (isLeft(p0, p1, point{x, y}) && isLeft(p1, p2, point{x, y}) &&
+            isLeft(p2, p0, point{x, y})) {
+          glVertex3f(x, y, 0.0);
         }
       }
-      glEnd();
     }
+    glEnd();
+    glColor3f(1.0, 1.0, 1.0);
   }
 
   glutSwapBuffers();
